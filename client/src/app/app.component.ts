cedee0456f7b2114baafe2d48e8e42d785292012
monidipbose <monidip.bose7@gmail.com>
2020-06-08 21:20:37 +0530

    angular basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (res: IPagination) => {
+        this.products = res.data;
+      },
+      // tslint:disable-next-line: no-shadowed-variable
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
